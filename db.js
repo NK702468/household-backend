70d06a1b27e8d33e4228c9df1e7fb69217d88f49
Nao Kameya <nao280523@gmail.com>
2026-04-05 23:44:02 +0900

    fix: change db connection to pool

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.DATABASE_URL)
+const db = mysql.createPool(process.env.DATABASE_URL);
 
-db.connect((err) => {
-    if(err) {
-        console.log("DB接続失敗", err);
-        return;
-    }
-    console.log("DB接続成功");
+db.getConnection((err, connection) => {
+  if (err) {
+    console.log("DB接続失敗", err);
+    return;
+  }
+  console.log("DB接続成功");
+  connection.release();
 });
 
 module.exports = db;
